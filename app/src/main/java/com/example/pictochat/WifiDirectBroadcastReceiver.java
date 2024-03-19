aa4227b924dcea1530d1f15cdfda92ad078ca9c2
Sam <Daman1599@gmail.com>
2024-03-19 14:05:59 -0400

    Added toast for wifi state change

--- app/src/main/java/com/example/pictochat/WifiDirectBroadcastReceiver.java
+++ app/src/main/java/com/example/pictochat/WifiDirectBroadcastReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.widget.Toast;
 
 public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
 
@@ -22,7 +23,14 @@ public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
         String action = intent.getAction();
 
         if(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)){
-            //do something
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+
+            if(state==WifiP2pManager.WIFI_P2P_STATE_ENABLED){
+                Toast.makeText(context, "WiFi is ON", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(context, "WiFi is OFF", Toast.LENGTH_SHORT).show();
+            }
         }
         else if(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)){
             //do something
