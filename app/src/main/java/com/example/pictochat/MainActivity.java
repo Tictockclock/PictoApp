6390ebd365dc3863fa7bffb1eaa7a21ad3153002
EvanBartel <Evanbartel6@gmail.com>
2024-03-26 16:22:46 -0400

    End of video 8

--- app/src/main/java/com/example/pictochat/MainActivity.java
+++ app/src/main/java/com/example/pictochat/MainActivity.java
@@ -28,6 +28,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
@@ -52,6 +54,10 @@ public class MainActivity extends AppCompatActivity {
     WifiP2pDevice[] deviceArray;
     static final int MESSAGE_READ = 1;
 
+    ServerClass serverClass;
+    ClientClass clientClass;
+    SendReceive sendReceive;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,7 +68,15 @@ public class MainActivity extends AppCompatActivity {
 
     Handler handler = new Handler(new Handler.Callback(){
         @Override
-        public boolean handleMessage(Message message) {
+        public boolean handleMessage(Message msg) {
+            switch (msg.what){
+                case MESSAGE_READ:
+                    byte[] readBuff = (byte[]) msg.obj;
+                    String tempMsg = new String(readBuff, 0, msg.arg1);
+                    read_msg_box.setText(tempMsg);
+                    break;
+
+            }
             return true;
         }
     });
@@ -137,6 +151,13 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = writeMsg.getText().toString();
+                sendReceive.write(msg.getBytes());
+            }
+        });
     }
 
     private void initialWork() { //initialize variables
@@ -203,9 +224,14 @@ public class MainActivity extends AppCompatActivity {
 
             if(wifiP2pInfo.groupFormed && wifiP2pInfo.isGroupOwner) {
                 connectionStatus.setText("Host");
+                serverClass = new ServerClass();
+                serverClass.start();
             }
             else if (wifiP2pInfo.groupFormed){
                 connectionStatus.setText("Client");
+                clientClass = new ClientClass(groupOwnerAddress);
+                clientClass.start();
+
             }
         }
     };
@@ -229,10 +255,58 @@ public class MainActivity extends AppCompatActivity {
             try {
                 serverSocket = new ServerSocket(8888);
                 socket = serverSocket.accept();
+                sendReceive = new SendReceive(socket);
+                sendReceive.start();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    private class SendReceive extends Thread{
+        private  Socket socket;
+        private InputStream inputStream;
+        private OutputStream outputStream;
+
+        public SendReceive(Socket skt){
+            socket = skt;
+            try{
+                inputStream = socket.getInputStream();
+                outputStream = socket.getOutputStream();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while (socket != null) {
+                try {
+                    bytes = inputStream.read(buffer);
+                    if (bytes > 0) {
+                        handler.obtainMessage(MESSAGE_READ, bytes, -1,  buffer).sendToTarget();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+        public void write(byte[] bytes) {
+            try {
+                outputStream.write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+
     }
 
     public class ClientClass extends Thread{
@@ -243,6 +317,8 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             try {
                 socket.connect(new InetSocketAddress(hostAdd,8888),500);
+                sendReceive = new SendReceive(socket);
+                sendReceive.start();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
